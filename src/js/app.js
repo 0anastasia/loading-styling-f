edad2ed968cac64465dc38a4ba31b88df27a850d
Anastasia <anastasia.chashch@mail.ru>
2023-10-04 23:12:36 +0500

    done

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import API from './api';
+import Controller from './controller';
+import Layout from './layout';
+
+const app = new Controller(new Layout(), new API());
+app.init();
+app.renderArticle();
